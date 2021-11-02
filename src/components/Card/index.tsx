b0530a6570fc764da62b5864b3e82eff124da0bb
baba <liu@babadeMacBook-Pro.local>
2021-11-02 18:52:23 +0800

    增加 card 测试

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,15 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2021-11-02 17:05:11
- * @LastEditTime: 2021-11-02 17:05:12
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-11-02 18:06:50
+ * @LastEditors: jack-pearson
  * @Description: In User Settings Edit
  * @FilePath: /angel-ui-example/src/components/Card/index.tsx
  */
 import React from "react";
-
+import { Card as CardComponent } from "angel-ui";
 const Card = () => {
-  return <div className="card">card</div>;
+  return <CardComponent className="card" title="测试"></CardComponent>;
 };
 
 export default Card;
